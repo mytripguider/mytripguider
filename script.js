41399e8a370f99bd4b0149a05a60171b880e9c6b
mytripguider <147905740+mytripguider@users.noreply.github.com>
2023-10-14 12:32:32 +0530

    Ultimate Travel is here
    Ultimate Travel is here

---
+++ script.js
@@ -0,0 +1,23 @@
+// photo slides on first page
+
+let slideIndex = 0;
+
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+showSlides();
+
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  window.scrollTo({
+      top: section.offsetTop,
+      behavior: 'smooth'
+  });
+}
